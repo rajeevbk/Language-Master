fe491968674458bb406c97c8a08d22b1efdc4646
Rajeev Kodippily <rkodippily@gmail.com>
2020-03-14 00:27:10 +0530

    main activity done

--- app/src/main/java/kodippily/rajeev/languagemaster/MainActivity.java
+++ app/src/main/java/kodippily/rajeev/languagemaster/MainActivity.java
@@ -1,14 +1,29 @@
 package kodippily.rajeev.languagemaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import  android.database.sqlite.*;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Button addPhrases;
+    private static Button displayPhrases;
+    private static Button editPhrases;
+    private static Button languageSubscription;
+    private static Button translate;
+
+    //Get database instance
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteConnection db = SQLiteConnection.getInstance();
+        db.test();
+
     }
 }
---
+++ app/src/main/java/kodippily/rajeev/languagemaster/Translate.java
@@ -0,0 +1,14 @@
+package kodippily.rajeev.languagemaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Translate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+    }
+}
